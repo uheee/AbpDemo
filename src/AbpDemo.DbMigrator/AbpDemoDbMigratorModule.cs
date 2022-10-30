cd5a12b8d967a1328365e1d75a6c6db5662ca080
Snowind <jinks.tao@gmail.com>
2022-10-30 11:22:28 +0800

    feat: initial commit

---
+++ src/AbpDemo.DbMigrator/AbpDemoDbMigratorModule.cs
@@ -0,0 +1,19 @@
+﻿using AbpDemo.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.Modularity;
+
+namespace AbpDemo.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AbpDemoEntityFrameworkCoreModule),
+    typeof(AbpDemoApplicationContractsModule)
+    )]
+public class AbpDemoDbMigratorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+    }
+}
---
+++ src/AbpDemo.HttpApi.Host/AbpDemoBrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace AbpDemo;
+
+[Dependency(ReplaceServices = true)]
+public class AbpDemoBrandingProvider : DefaultBrandingProvider
+{
+    public override string AppName => "AbpDemo";
+}
